c7ed826f7d89c9c40958713ed17f98778f1dbe69
Cédryc Ruth <ceruth@epfc.eu>
2024-03-13 12:25:47 +0100

    CRUD Location

---
+++ src/Form/LocationType.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Locality;
+use App\Entity\Location;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class LocationType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('slug')
+            ->add('designation')
+            ->add('address')
+            ->add('website')
+            ->add('phone')
+            ->add('locality', EntityType::class, [
+                'class' => Locality::class,
+                'choice_label' => 'id',
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Location::class,
+        ]);
+    }
+}
